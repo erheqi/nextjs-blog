4709825efd8ae25e7cfdb511491241f6cd2a9846
erheqi <50977864+erheqi@users.noreply.github.com>
2023-04-08 16:43:59 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,1 +1,17 @@
+export const getGlobalData = () => {
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
+    : 'erli';
+  const blogTitle = process.env.BLOG_TITLE
+    ? decodeURI(process.env.BLOG_TITLE)
+    : 'BLOG';
+  const footerText = process.env.BLOG_FOOTER_TEXT
+    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+    : 'All rights reserved.';
 
+  return {
+    name,
+    blogTitle,
+    footerText,
+  };
+};
